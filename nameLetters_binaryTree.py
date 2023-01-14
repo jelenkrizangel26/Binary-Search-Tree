e9a079c721136c81120353770c1c1c4ab89c25a5
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2023-01-14 12:03:46 +0800

    Adding the other possible cases and making the left and right subtree

--- nameLetters_binaryTree.py
+++ nameLetters_binaryTree.py
@@ -1,5 +1,23 @@
+#Making the lesson on the first vid using my name as the string: JELEN KRIZ ANGEL PERALTA MAMPUSTI
+#Will include the search and in order traversal.
+
 class BinarySearchTreeNode:
     def __init__(self,data):
         self.data = data
         self.left = None
         self.right = None
+    #for if node already exist it would not repeat it
+    def add_me(self, data):
+        if data == self.data:
+            return 
+    #For the left and right subtree
+        if data < self.data:
+            if self.left:
+                self.left.add_me(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_me(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
